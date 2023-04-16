1e8f34d8dda10a2cef86e303ba0fc0c32ec6db7c
hamim <hamim4389@gmail.com>
2023-04-16 16:03:31 +0600

    Added a message in the SceondVC uiLabel

--- GitPractice/SecondVC.swift
+++ GitPractice/SecondVC.swift
@@ -9,10 +9,14 @@ import UIKit
 
 class SecondVC: UIViewController {
 
+    @IBOutlet var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        myLabel.text = "Either button 1 or  2 was clicked"
+        
     }
     
 
